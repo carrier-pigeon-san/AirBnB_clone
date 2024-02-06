cc852ea8d8449a39c904fd5992762322a5268045
CHIGODI <chiegody254@gmail.com>
2024-02-06 15:45:22 +0300

    Fix : give default times if kwargs doesnt provide convert created_at and updated_at in the to_dict to str

--- models/base_model.py
+++ models/base_model.py
@@ -17,18 +17,20 @@ class BaseModel:
         initialises instaces of the base class
         """
         if kwargs:
-            del kwargs['__class__']
-            created_at = kwargs['created_at']
-            updated_at = kwargs['updated_at']
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            created_at = kwargs.get('created_at', datetime.now().isoformat())
+            updated_at = kwargs.get('updated_at', datetime.now().isoformat())
             kwargs['created_at'] = datetime.strptime(created_at,
                                                      "%Y-%m-%dT%H:%M:%S.%f")
             kwargs['updated_at'] = datetime.strptime(updated_at,
                                                      "%Y-%m-%dT%H:%M:%S.%f")
+
             for key, value in kwargs.items():
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
 
     def __str__(self):
@@ -50,4 +52,13 @@ class BaseModel:
         """
         instance_dict = {'__class__': self.__class__.__name__}
         instance_dict.update(self.__dict__)
+        created_at = instance_dict.get('created_at')
+        updated_at = instance_dict.get('updated_at')
+        if created_at is not None:
+            instance_dict['created_at'] = created_at.isoformat()
+
+        if updated_at is not None:
+            instance_dict['updated_at'] = updated_at
+        elif updated_at is not None and updated_at != created_at:
+            instance_dict['updated_at'] = updated_at.isoformat()
         return instance_dict
