bca9fa0a71d7afd066696ea0dbabd5d9db0d048b
Sandy Obare <obaresandy@gmail.com>
2024-02-07 08:41:28 +0000

    console.py do create method initial commit

--- console.py
+++ console.py
@@ -9,7 +9,7 @@ HBNBCommand class. The class is primarily intended to manipulate objects in a
 file storage.
 """
 
-
+from airbnb_resc.base_model import BaseModel
 import cmd
 
 
@@ -20,6 +20,20 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = '(hbtn) '
+
+    def do_create(self, class_name=None):
+        """
+        Creates a new instance of class BaseModel, saves it (to a JSON file)
+        and prints the id.
+        """
+        if not class_name:
+            print("** class name missing **")
+        elif class_name in globals() and type(globals()[class_name]) is type:
+            instance = globals()[class_name]()
+            instance.save()
+            print(instance.id)
+        else:
+            print("** class doesn't exist **")
 
     def do_EOF(self, line):
         """Handles the end-of-file marker"""
