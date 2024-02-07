6677e262875c7faaa2e9ec15ad6413a505b5ac2c
CHIGODI <chiegody254@gmail.com>
2024-02-07 16:06:43 +0300

    Document script

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """
+Initialization Script for File Storage
+
+This script initializes a FileStorage instance and reloads
+data from a JSON file.
 """
 
 from .engine.file_storage import FileStorage
