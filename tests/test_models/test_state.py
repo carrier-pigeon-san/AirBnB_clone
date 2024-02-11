c5adf70d4302ae056b6bdf998de6c120e74e5fe3
Sandy Obare <obaresandy@gmail.com>
2024-02-11 15:24:55 +0000

    test_state.py Documentation of methods added Addition of method to test initialization of object attributes with **kwargs parameter

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -8,19 +8,22 @@ from models.state import State
 import unittest
 
 
-class TestCity(unittest.TestCase):
+class TestState(unittest.TestCase):
     """
     defines methods that test attributes and methods of object of class State
     """
 
-    def test_city_name(self):
+    def test_state_name(self):
+        """Tests initialization and data type of 'name' class attribute"""
         state = State()
         self.assertEqual(type(state.name), str)
+        self.assertRaises(TypeError, state.name, 3)
 
         state.name = "Washington"
         self.assertEqual(state.name, 'Washington')
 
     def test_inherited_attributes(self):
+        """Tests initialization and values of inherited attributes"""
         state = State()
         state0 = State()
         self.assertNotEqual(state.id, state0.id)
@@ -28,23 +31,46 @@ class TestCity(unittest.TestCase):
         self.assertGreaterEqual(state.updated_at, state.created_at)
 
     def test_str_function(self):
+        """Tests the output value of inherited '__str__' method"""
         state = State()
         state.name = "Washington"
         state_str = state.__str__()
         self.assertEqual(state_str, f"[State] ({state.id}) {state.__dict__}")
 
     def test_to_dict_function(self):
+        """Tests the output value of inherited 'to_dict' method"""
         state = State()
         state.name = "Washington"
         self.assertEqual(state.to_dict(),
-                {
-                    '__class__': 'State',
-                    'id': state.id,
-                    'created_at': state.created_at.isoformat(),
-                    'updated_at': state.updated_at.isoformat(),
-                    'name': 'Washington'
-                    }
+                         {
+                             '__class__': 'State',
+                             'id': state.id,
+                             'created_at': state.created_at.isoformat(),
+                             'updated_at': state.updated_at.isoformat(),
+                             'name': 'Washington'
+                             }
+                         )
+
+    def test_class_state_with_kwargs(self):
+        """Tests initialization of State class attributes with kwargs"""
+        kwargs = {
+                '__class__': 'MyModel',
+                'id': '12121212',
+                'created_at': '2019-06-27T09:17:08.031275',
+                'updated_at': '2019-06-27T09:17:08.031275',
+                'name': 'Washington'
+                }
+        state = State(**kwargs)
+        self.assertEqual(state.to_dict()['__class__'], 'State')
+        self.assertEqual(state.id, '12121212')
+        self.assertEqual(
+                state.created_at, datetime(2019, 6, 27, 9, 17, 8, 31275)
                 )
+        self.assertEqual(
+                state.updated_at, datetime(2019, 6, 27, 9, 17, 8, 31275)
+                )
+        self.assertEqual(state.name, 'Washington')
+
 
 if __name__ == '__main__':
     unittest.main()
